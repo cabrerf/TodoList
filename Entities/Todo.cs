b7fd62d71924874a5874fe4c5d1a3fce60615a6f
Federico Cabrera <federico.cabrera@ext.sonda.com>
2024-06-19 19:34:45 -0300

    Add project files.

---
+++ Entities/Todo.cs
@@ -0,0 +1,10 @@
+﻿namespace Entities
+{
+    public class Todo
+    {
+
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+
+    }
+}
---
+++ TodoList/Controllers/TodosController.cs
@@ -0,0 +1,106 @@
+﻿using Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoList.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodosController : ControllerBase
+    {
+
+        List<Todo> todos = new List<Todo>
+        {
+            new Todo { Id = 1, Descripcion = "Study" },
+            new Todo { Id = 2, Descripcion = "Drink coffee" },
+            new Todo { Id = 3, Descripcion = "Play football" }
+        };
+
+
+        public TodosController()
+        {
+           
+        }
+
+        [HttpGet]
+        [Route("Get")]
+        public async Task<IActionResult> Get()
+        {
+            
+            return Ok(todos);
+        }
+
+        [HttpGet]
+        [Route("GetId")]
+        public async Task<IActionResult> GetId(int id)
+        {
+            var miElemento = todos.FirstOrDefault(todo => todo.Id == id);
+            
+            return Ok(miElemento);
+        }
+
+        [HttpPost]
+        [Route("Create")]
+
+        public async Task<IActionResult> Create(Todo todo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return UnprocessableEntity(ModelState);
+            }
+
+            var t=  todos.LastOrDefault();
+
+            todo.Id = t.Id + 1;
+           
+            todos.Add(todo);
+
+
+            return Ok(todo);
+        }
+
+        [HttpPut]
+        [Route("Put")]
+
+        public async Task<IActionResult> Put(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return UnprocessableEntity(ModelState);
+            }
+
+
+            var miElemento = todos.FirstOrDefault(todo => todo.Id == id);
+            miElemento.Descripcion = "change";
+
+            return Ok(miElemento);
+
+
+
+        }
+
+
+        [HttpDelete]
+        [Route("Delete")]
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return UnprocessableEntity(ModelState);
+            }
+
+            var miElemento = todos.RemoveAll(todo => todo.Id == id);
+
+            return Ok("Id" + id.ToString() + "removed");
+
+
+        }
+
+
+
+
+
+    }
+}
